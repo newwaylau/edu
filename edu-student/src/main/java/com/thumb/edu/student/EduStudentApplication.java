cf9448cb2faf5a15ccd453a00eb639e8a685876c
heaton <newwaylau@hotmail.com>
2019-09-19 19:15:16 +0800

    init

---
+++ edu-student/src/main/java/com/thumb/edu/student/EduStudentApplication.java
@@ -0,0 +1,13 @@
+package com.thumb.edu.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduStudentApplication.class, args);
+    }
+
+}
