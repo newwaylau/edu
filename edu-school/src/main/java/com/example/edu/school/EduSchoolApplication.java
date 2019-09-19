218fa6bc4afcfbd428f17098fc4c479bbdea6075
heaton <newwaylau@hotmail.com>
2019-09-19 20:56:31 +0800

    init

---
+++ edu-school/src/main/java/com/example/edu/school/EduSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.edu.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduSchoolApplication.class, args);
+    }
+
+}
