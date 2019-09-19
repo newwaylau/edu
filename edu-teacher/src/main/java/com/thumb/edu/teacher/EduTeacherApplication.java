2c4b60de2388827080b5f962ac4d8244b2636d70
heaton <newwaylau@hotmail.com>
2019-09-19 20:54:10 +0800

    init

---
+++ edu-teacher/src/main/java/com/thumb/edu/teacher/EduTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.thumb.edu.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduTeacherApplication.class, args);
+    }
+
+}
---
+++ edu-teacher/src/test/java/com/thumb/edu/teacher/EduTeacherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thumb.edu.teacher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EduTeacherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
